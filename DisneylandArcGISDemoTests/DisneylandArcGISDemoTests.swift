acebb72bf66b1a0ad0179d126c02ebaabe2b5dfe
Jeremiah <jjessel@jcubedapps.com>
2015-06-01 20:56:31 -0700

    Initial Commit

---
+++ DisneylandArcGISDemoTests/DisneylandArcGISDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  DisneylandArcGISDemoTests.swift
+//  DisneylandArcGISDemoTests
+//
+//  Created by Jeremiah Jessel on 6/1/15.
+//  Copyright (c) 2015 JCubedApps. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DisneylandArcGISDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
